2c17129088d25926d94f722f98e9af19fede4c4a
Sherry Ummen <sherry.ummen@napa.fi>
2019-06-26 11:29:22 +0300

    updated the problematic call

--- MongodbBugReport_ArithmeticException/Program.cs
+++ MongodbBugReport_ArithmeticException/Program.cs
@@ -1,5 +1,7 @@
 ﻿using MongoDB.Driver;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MongodbBugReport_ArithmeticException
 {
@@ -12,18 +14,30 @@ namespace MongodbBugReport_ArithmeticException
             string remoteServer = "nsmongodb01";
             client = new MongoClient("mongodb://" + remoteServer + ":27017");
             ProbelmaticCall();
+            //NonProbelmaticCall();
+            Console.WriteLine("Reached here all is OK");
             Console.ReadKey();
         }
 
         static void ProbelmaticCall()
         {
             FloatingPointControl.TurnOnFpExceptions();
-            var list = client.ListDatabaseNames().ToList();
+            var list = client.GetDatabaseNames().ToList();
         }
         static void NonProbelmaticCall()
         {
-            var list = client.ListDatabaseNames().ToList();
+            var list = client.GetDatabaseNames().ToList();
         }
+    }
 
+    static class Extension
+    {
+        public static IEnumerable<string> GetDatabaseNames(this IMongoClient client)
+        {
+
+            return client.ListDatabases()
+                .ToList()
+                .Select(db => db.GetValue("name").AsString);
+        }
     }
 }
